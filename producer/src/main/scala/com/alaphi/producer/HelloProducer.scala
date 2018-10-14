0ffbefb9e1f438f3223a46f2edfbfcfa2bf4aa30
alanp <alanphillips78@gmail.com>
2018-10-14 23:17:06 +0100

    Remove unused code

--- producer/src/main/scala/com/alaphi/producer/HelloProducer.scala
+++ producer/src/main/scala/com/alaphi/producer/HelloProducer.scala
@@ -21,12 +21,6 @@ object HelloProducer extends App {
 
   val producer = KafkaProducer[String,String](producerCfg, scheduler)
 
-  // For sending one message
-  val recordMetadataF = producer.send("hello-topic", "hello1")
-
-  // For closing the producer connection
-  val closeF = producer.close()
-
   val publish = for {
     _ <- producer.send("hello-topic", HelloThere("Hellooo", "there").asJson.spaces2).delayExecution(30 seconds)
     close <- producer.close()
